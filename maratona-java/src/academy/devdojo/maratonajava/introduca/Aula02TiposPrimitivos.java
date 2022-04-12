e108b7e5a068e4e046ee789cd4aa2c670f01a556
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2022-04-12 20:04:00 -0300

    Revert "adicionando data"
    This reverts commit e8108c608a9ee2bb1590e8da152e89fd6b40b417.

--- maratona-java/src/academy/devdojo/maratonajava/introduca/Aula02TiposPrimitivos.java
+++ maratona-java/src/academy/devdojo/maratonajava/introduca/Aula02TiposPrimitivos.java
@@ -1,10 +1,5 @@
 package academy.devdojo.maratonajava.introduca;
 
-import java.text.SimpleDateFormat;
-import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 public class Aula02TiposPrimitivos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,17 +17,13 @@ public class Aula02TiposPrimitivos {
 		
 		short idadeShort = (short) 32769;
 		
-		Date dataNascimento = new Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		String formattedDate = dateFormat.format(dataNascimento);
-		
 		
 		
 		var numero = 10;
 		var nome = "string";
 		var Double = 10000D;
 		
-		System.out.println(formattedDate);
+		System.out.println(Double);
 
 		
 	}
